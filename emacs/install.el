afbae8929bfc1a558b0f3e19a8fe01b6b6a4faf6
Keisuke Fukuda <keisukefukuda@gmail.com>
2013-04-17 16:08:34 +0900

    added a few packages

--- emacs/install.el
+++ emacs/install.el
@@ -15,10 +15,14 @@
   (when (not (package-installed-p pkg))
     (package-install pkg)))
 
-(check-install 'anything)
 (check-install 'cmake-mode)
 (check-install 'js2-mode)
 (check-install 'markdown-mode)
 (check-install 'markdown-mode)
+(check-install 'helm)
+(check-install 'slime)
+(check-install 'haskell-mode)
+(check-install 'flymake-haskell-multi)
 
 (check-install 'flymake-jslint)
+
