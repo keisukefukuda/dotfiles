faefc06b316684912e2d8c1fcd521aa7f32fc4cc
Keisuke Fukuda <fukuda@matsulab.is.titech.ac.jp>
2014-12-22 14:39:47 +0900

    customized C++-mode indentation

--- emacs/emacs.d/01-key-misc.el
+++ emacs/emacs.d/01-key-misc.el
@@ -20,3 +20,10 @@
 (show-paren-mode t)
 (setq transient-mark-mode t)
 
+(add-hook 'c++-mode-hook
+          '(lambda()
+             (c-set-style "stroustrup")
+             (setq indent-tabs-mode nil)
+             (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
+             (c-set-offset 'arglist-close 0) ; 引数リストの閉じ括弧はインデントしない
+             ))
