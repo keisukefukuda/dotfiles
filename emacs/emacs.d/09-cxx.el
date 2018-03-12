dbca53de6668ca3795ff0407d186e97eb68272a4
Keisuke Fukuda <kfukuda@preferred.jp>
2018-03-12 14:27:48 +0900

    C++ formatter change

--- emacs/emacs.d/09-cxx.el
+++ emacs/emacs.d/09-cxx.el
@@ -4,16 +4,23 @@
 (setq auto-mode-alist
       (append '(("\\.cu" . c++-mode)) auto-mode-alist))
 
+(setq-default c-basic-offset 4)
+
 ;; Coding style
-(load "google-c-style.el")
+(setq clang-format-style "{BasedOnStyle: Google, AccessModifierOffset: -4, ColumnLimit: 140, IndentWidth: 4, DerivePointerAlignment: false, PointerAlignment: Left}")
+
+;(add-hook 'c-mode-common-hook
+          ;(function (lambda()
+                    ;(add-hook 'after-save-hook
+                                        ;'clang-format-buffer))))
+
 (add-hook 'c-mode-common-hook
-          '(lambda()
-             (google-set-c-style)))
+          (lambda()
+            (c-set-offset 'inextern-lang 0)
+            (c-set-offset 'innamespace 0)))
 
 (setq auto-mode-alist
       (append '(("\\.h" . c++-mode)) auto-mode-alist))
-
-(c-set-offset 'inextern-lang 0)
 
 ;; http://qiita.com/alpha22jp/items/90f7f2ad4f8b1fa089f4
 ;; http://qiita.com/MitsutakaTakeda/items/2f526a85ad39424a8363
