af477a65ca9deb31d6b93e443db169fb50500c1a
Keisuke Fukuda <keisukefukuda@gmail.com>
2018-08-28 15:12:08 +0900

    Updated C/C++ env

--- emacs/emacs.d/09-cxx.el
+++ emacs/emacs.d/09-cxx.el
@@ -4,16 +4,16 @@
 (setq auto-mode-alist
       (append '(("\\.cu" . c++-mode)) auto-mode-alist))
 
-;; Coding style
-(load "google-c-style.el")
-(add-hook 'c-mode-common-hook
-          '(lambda()
-             (google-set-c-style)))
-
 (setq auto-mode-alist
       (append '(("\\.h" . c++-mode)) auto-mode-alist))
 
-(c-set-offset 'inextern-lang 0)
+;; Coding style
+(add-hook 'c-mode-common-hook
+          '(lambda()
+             (google-set-c-style)
+             (setq c-basic-offset 4)
+             (c-offsets-alist . ((innamespace . -)
+                                 (inextern-lang . 0)))))
 
 ;; http://qiita.com/alpha22jp/items/90f7f2ad4f8b1fa089f4
 ;; http://qiita.com/MitsutakaTakeda/items/2f526a85ad39424a8363
@@ -33,29 +33,29 @@
   (define-key company-search-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection))
 
-(when (require 'flycheck nil 'noerror)
-  (custom-set-variables
-   ;; Show popup error window
-   '(flycheck-display-errors-function
-     (lambda (errors)
-       (let ((messages (mapcar #'flycheck-error-message errors)))
-         (popup-tip (mapconcat 'identity messages "\n")))))
-   '(flycheck-display-errors-delay 0.5))
-  (define-key flycheck-mode-map (kbd "C-M-n") 'flycheck-next-error)
-  (define-key flycheck-mode-map (kbd "C-M-p") 'flycheck-previous-error)
-  (add-hook 'c-mode-common-hook 'flycheck-mode))
+;; (when (require 'flycheck nil 'noerror)
+;;   (custom-set-variables
+;;    ;; Show popup error window
+;;    '(flycheck-display-errors-function
+;;      (lambda (errors)
+;;        (let ((messages (mapcar #'flycheck-error-message errors)))
+;;          (popup-tip (mapconcat 'identity messages "\n")))))
+;;    '(flycheck-display-errors-delay 0.5))
+;;   (define-key flycheck-mode-map (kbd "C-M-n") 'flycheck-next-error)
+;;   (define-key flycheck-mode-map (kbd "C-M-p") 'flycheck-previous-error)
+;;   (add-hook 'c-mode-common-hook 'flycheck-mode))
 
-(eval-after-load "flycheck"
-  '(progn
-     (when (locate-library "flycheck-irony")
-       (flycheck-irony-setup))))
+;; (eval-after-load "flycheck"
+;;   '(progn
+;;      (when (locate-library "flycheck-irony")
+;;        (flycheck-irony-setup))))
 
-(eval-after-load "irony"
-  '(progn
-     (custom-set-variables '(irony-additional-clang-options '("-std=c++11")))
-     (add-to-list 'company-backends 'company-irony)
-     (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-     (add-hook 'c-mode-common-hook 'irony-mode)))
+;; (eval-after-load "irony"
+;;   '(progn
+;;      (custom-set-variables '(irony-additional-clang-options '("-std=c++11")))
+;;      (add-to-list 'company-backends 'company-irony)
+;;      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;      (add-hook 'c-mode-common-hook 'irony-mode)))
 
 (when (require 'rtags nil 'noerror)
   (add-hook 'c-mode-common-hook
